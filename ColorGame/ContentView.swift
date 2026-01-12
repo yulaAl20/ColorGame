f29bf0c2c21952d4134c46c0a61d1d89f1bd3fd4
yulaAl20 <yulani.alwis0@gmail.com>
2026-01-12 13:57:32 +0530

    Initial Commit

---
+++ ColorGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorGame
+//
+//  Created by cobsccomp242p-066 on 2026-01-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
